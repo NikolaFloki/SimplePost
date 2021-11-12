22da823687f41b99b3940fe4160d0dfd5e6bd322
NikolaFloki <dragan@UX305CA-7f2afe4d.localdomain>
2021-11-13 00:23:41 +0100

    update template

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Posts
 
 def home_page(request):
-    return render(request,'home_page.html', {})
+    all_posts = Posts.objects.all()[:10]   #ogranicili smo postove na 10 da ne izlistava 100 komada na naslovnoj strani
+    list_all_posts = {'all_posts' : all_posts}
+    return render(request, 'home_page.html', list_all_posts)
+
+
+def details(request, id):
+    one_result = Posts.objects.get(id=id)
+    one_post = {"one_result" : one_result}
+    return render(request, 'details.html', one_post)
+
+
+def admin(request):
+    response = redirect('admin')
+    return response
